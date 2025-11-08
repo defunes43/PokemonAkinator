ac3bd533928ff4fe265457d165042bb16c91700b
Maxime Visconte <maxime@visconte.fr>
2025-11-08 22:37:07 +0100

    build with path

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,11 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const repoName = 'PokemonAkinator'; 
+const isCapacitorBuild = process.env.CAPACITOR_BUILD === 'true';
+
+const base = isCapacitorBuild ? '' : `/${repoName}`; 
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
@@ -8,9 +13,9 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		paths: {
+            base: base, 
+        },
 		adapter: adapter({
       // Toutes les pages sont pré-rendues
       fallback: 'index.html' // Important pour une SPA
