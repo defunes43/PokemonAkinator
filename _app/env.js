5c7cb9648815986812498a797fd9397ccd91aa47
defunes43 <mvisconte@wanadoo.fr>
2025-11-08 22:08:05 +0000

    Deploying to gh-pages from @ defunes43/PokemonAkinator@260333787db68500c95a40bf684270086012ea28 🚀

--- _app/env.js
+++ _app/env.js
@@ -1,1 +1,1 @@
-export const env={"PUBLIC_BASE_PATH":"/PokemonAkinator"}
+export const env={"PUBLIC_BASE_PATH":"/"}
---
+++ _app/immutable/nodes/2.Cishu0qR.js
@@ -0,0 +1,1 @@
+import{_ as m}from"../chunks/2pngOHfL.js";export{m as component};
